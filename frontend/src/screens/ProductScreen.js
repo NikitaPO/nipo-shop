b9759356946ff69d6c78b5e0bec88572ec6429d3
NikitaPO <gigzim345@gmail.com>
2020-10-28 09:40:57 +0300

    added reducer productDetails

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,59 +1,70 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, Col, Image, ListGroup, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
+import { listProductsDetails } from "../actions/productsActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProductScreen = ({ match }) => {
-  let [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, product, error } = productDetails;
 
   useEffect(() => {
-    axios
-      .get(`/api/products/${match.params.id}`)
-      .then((res) => setProduct(res.data))
-      .catch(() => console.log("Can't find product"));
-  }, [match]);
+    dispatch(listProductsDetails(match.params.id));
+  }, [match, dispatch]);
 
   let isInStock = product.countInStock > 0;
 
   return (
     <>
-      <Row className="my-3">
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-          <Link type="button" className="btn btn-primary mt-3" to="/">
-            Go back
-          </Link>
-        </Col>
-        <Col md={6}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Status:{" "}
-              {isInStock ? (
-                <span className="text-success">In stock</span>
-              ) : (
-                <span className="text-danger">Out of stock</span>
-              )}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Price: <b>${product.price}</b>
-            </ListGroup.Item>
-            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
-          </ListGroup>
-          <Button className={"btn-block btn-success"} disabled={!isInStock}>
-            Add to card
-          </Button>
-        </Col>
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row className="my-3">
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+            <Link type="button" className="btn btn-primary mt-3" to="/">
+              Go back
+            </Link>
+          </Col>
+          <Col md={6}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Status:{" "}
+                {isInStock ? (
+                  <span className="text-success">In stock</span>
+                ) : (
+                  <span className="text-danger">Out of stock</span>
+                )}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Price: <b>${product.price}</b>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
+            </ListGroup>
+            <Button className={"btn-block btn-success"} disabled={!isInStock}>
+              Add to card
+            </Button>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReduser } from "./reducers/productsRedusers";
+import {
+  productDetailsReduser,
+  productListReduser,
+} from "./reducers/productsRedusers";
 
-const reducer = combineReducers({ productList: productListReduser });
+const reducer = combineReducers({
+  productList: productListReduser,
+  productDetails: productDetailsReduser,
+});
 
 const initialState = {};
 
