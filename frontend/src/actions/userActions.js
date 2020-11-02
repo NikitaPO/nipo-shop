89b95b8faddc0c84e03bfb51749e0a1ee88a6e40
NikitaPO <gigzim345@gmail.com>
2020-11-02 14:08:15 +0300

    added login screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
 } from "../constans/userConstans";
 
-export const userLogin = (email, password) => async (dispatch) => {
+export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST });
 
